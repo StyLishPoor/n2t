7a0b3856e6162d1dccad70541c0e360b5885903f
StyLishPoor <isstykkgd@gmail.com>
2020-03-15 20:11:40 +0900

    complete 04

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,23 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+    @R2
+    M=0
+(LOOP)
+    @R1
+    D=M
+    //R1の値が0になるまで加算し続ける
+    @END
+    D;JEQ
+    @R0
+    D=M
+    @R2
+    M=M+D
+    @R1
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
+
